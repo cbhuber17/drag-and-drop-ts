9af9a9476f990fc73e994dd5cde3e6888d654dea
Colin Huber <cbhuber@gmail.com>
2023-08-17 11:21:05 -0600

    feat: page render via ts/js.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-// Code goes here!
+class ProjectInput {
+  // Select specific template
+  templateElement: HTMLTemplateElement;
+
+  // Where to host (in the app div)
+  hostElement: HTMLDivElement;
+
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const projectInput = new ProjectInput();
