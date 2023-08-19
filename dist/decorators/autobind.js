145680e3a7db42fc730fb500fe97cccbb7c00f24
Colin Huber <cbhuber@gmail.com>
2023-08-18 23:08:13 -0600

    env: adding es6 dist.

---
+++ dist/decorators/autobind.js
@@ -0,0 +1,12 @@
+export function autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjustedDescriptor;
+}
+//# sourceMappingURL=autobind.js.map
